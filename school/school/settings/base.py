1faec6dd138c0da7d2b10a41cdf9619a7205d571
mande <asemakahamande01@gmail.com>
2026-03-18 15:41:37 +0100

    environment variables added

--- school/school/settings/base.py
+++ school/school/settings/base.py
@@ -97,7 +97,10 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': os.getenv('DB_SSLMODE', 'require'),  # ← add this
+        },
     }
 }
 
